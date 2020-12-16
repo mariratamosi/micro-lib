f258938f31c7f1efc11319e6c0b8dad9c8935ee6
marira <rongrazzo@gmail.com>
2020-12-17 01:46:31 +0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import uniqueRandomArray from "unique-random-array";
+var myVars = [
+  "1",
+  "12",
+  "13",
+  "1bf",
+  "15t4",
+  "156",
+  "1658",
+  "178",
+  "189",
+  "165",
+  "1421",
+  "1123",
+  "1537",
+  "16",
+  "15",
+  "134",
+];
+export const all = myVars;
+export const random = uniqueRandomArray(myVars);
