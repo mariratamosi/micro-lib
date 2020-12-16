324c5992d4d40d87367c449aed66cec67f99b456
marira <rongrazzo@gmail.com>
2020-12-17 01:47:41 +0600

    from Json

--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
 import uniqueRandomArray from "unique-random-array";
-var myVars = [
-  "1",
-  "12",
-  "13",
-  "1bf",
-  "15t4",
-  "156",
-  "1658",
-  "178",
-  "189",
-  "165",
-  "1421",
-  "1123",
-  "1537",
-  "16",
-  "15",
-  "134",
-];
+var myVars = require("./ints.json");
 export const all = myVars;
 export const random = uniqueRandomArray(myVars);
